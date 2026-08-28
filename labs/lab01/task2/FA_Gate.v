f7213e356da870f4b9e3f5915abd1a93e04f1f97
prisha-karn <f20250025@pilani.bits-pilani.ac.in>
2026-08-28 08:56:43 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -27,9 +27,19 @@ module FA_Gate(
   wire ps, pc1, pc2;
 
   xor (ps,  a,   b);
+  xor #(2,5) (ps, a, b);
+
   and (pc1, a,   b);
+  and #(2,5) (pc1, a, b);
+
   xor (sum, cin, ps);
+  xor #(2,5) (sum, cin, ps);
+
   and (pc2, cin, ps);
+  and #(2,5) (pc2, cin, ps);
+
   or  (cout, pc1, pc2);
+  or #(2,5) (cout, pc1, pc2);
+
 
 endmodule
